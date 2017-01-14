b3c30cd08f36cde3b940b1fc3f402eda910981ce
Heinrich Schuchardt <xypron.glpk@gmx.de>
2017-01-14 21:23:37 +0100

    examples/tls/multiseed.c: check TLS is available
    Check that thread local memory is available.
    Add more code comments.
    
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- examples/tls/multiseed.c
+++ examples/tls/multiseed.c
@@ -46,6 +46,7 @@
 
 #define BUFLEN 256
 
+/* Task descriptor */
 struct task {
    pthread_t tid;
    char *filename;
@@ -56,14 +57,19 @@ struct task {
    jmp_buf jmp;
 };
 
+/* Mutex for console output */
 pthread_mutex_t mutex;
 
+/* Console output handler */
 int term_hook(void *info, const char *text)
 {
    struct task *task = (struct task *) info;
    size_t len = strlen(text);
 
+   /* Lock mutex so this is the only task creating console output. */
    pthread_mutex_lock(&mutex);
+
+   /* Append the new text to the buffer. */
    if (task->pos + len > BUFLEN) {
       printf("%02d-%05d %s%s", task->seed, ++task->line, task->buf, text);
       task->pos = 0;
@@ -72,15 +78,22 @@ int term_hook(void *info, const char *text)
       strcpy(task->buf + task->pos, text);
       task->pos += len;
    }
+
+   /* If a complete line is available, send it to the console. */
    if (strchr(task->buf, '\n')) {
       printf("%02d-%05d %s", task->seed, ++task->line, task->buf);
       task->pos = 0;
       task->buf[0] = 0;
    }
+
+   /* Unlock the mutex. */
    pthread_mutex_unlock(&mutex);
+
+   /* Disable default output. */
    return -1;
 }
 
+/* Error handler */
 void error_hook(void *info)
 {
    struct task *task = (struct task *) info;
@@ -98,47 +111,59 @@ void worker(void *arg)
    glp_tran *tran;
    glp_iocp iocp;
 
-   glp_error_hook(error_hook, task);
-
    if (setjmp(task->jmp)) {
+      /* If an error is caught leave the function. */
       return;
    }
 
+   /* Set the error handler. */
+   glp_error_hook(error_hook, task);
+
+   /* Set the console output handler. */
    glp_term_hook(term_hook, arg);
 
    glp_printf("Seed %02d\n", task->seed);
 
+   /* Create the problem object. */
    lp = glp_create_prob();
+   if (!lp) {
+      glp_error("Out of memory\n");
+   }
+
+   /* Create the MathProg translator workspace. */
    tran = glp_mpl_alloc_wksp();
+   if (!lp) {
+      glp_error("Out of memory\n");
+   }
+
+   /* Set the pseudo random number generator seed. */
    glp_mpl_init_rand(tran, task->seed);
 
+   /* Read the model file. */
    ret = glp_mpl_read_model (tran, task->filename, GLP_OFF);
    if (ret != 0) {
-      glp_mpl_free_wksp (tran);
-      glp_delete_prob(lp);
-      glp_printf("Model %s not valid\n", task->filename);
-      glp_free_env();
+      glp_error("Model %s is not valid\n", task->filename);
    }
 
+   /* Generate the model. */
    ret = glp_mpl_generate(tran, NULL);
    if (ret != 0) {
-      glp_mpl_free_wksp (tran);
-      glp_delete_prob(lp);
-      glp_printf("Cannot generate model %s\n", task->filename);
-      glp_free_env();
+      glp_error("Cannot generate model %s\n", task->filename);
    }
 
+   /* Build the problem. */
    glp_mpl_build_prob(tran, lp);
 
+   /* Solve the problem. */
    glp_init_iocp(&iocp);
    iocp.presolve = GLP_ON;
-
    ret = glp_intopt(lp, &iocp);
-
    if (ret == 0) {
+      /* Execute the post solve part of the model. */
       glp_mpl_postsolve(tran, lp, GLP_MIP);
    }
 
+   /* Release the memory. */
    glp_mpl_free_wksp (tran);
    glp_delete_prob(lp);
 
@@ -170,6 +195,15 @@ int main(int argc, char *argv[])
    int i, n, rc;
    struct task *tasks;
 
+   /* Make sure thread local memory is used by the GLPK library. */
+   if (!glp_config("TLS")) {
+      printf("The loaded GLPK library does not support thread local memory.\n"
+        "You need a version of the library configured with "
+	"--enable-reentrant=yes to run this program.\n");
+      exit(EXIT_FAILURE);
+   }
+
+   /* Check program arguments. */
    if (argc != 3) {
       printf("Usage %s filename threads\n"
              "  filename - MathProg model file\n"
@@ -178,6 +212,7 @@ int main(int argc, char *argv[])
       exit(EXIT_FAILURE);
    }
 
+   /* Parse the arguments. */
    n = atoi(argv[2]);
    if (n > 50) {
       printf("Number of threads is to high (> 50).\n");
@@ -188,14 +223,17 @@ int main(int argc, char *argv[])
       exit(EXIT_FAILURE);
    }
 
+   /* Allocate memory for the task descriptors. */
    tasks = calloc(n, sizeof(struct task));
    if (!tasks) {
       printf("Out of memory");
       exit(EXIT_FAILURE);
    }
 
+   /* Create a mutex for console output. */
    pthread_mutex_init(&mutex, NULL);
 
+   /* Create the threads. */
    for (i = 0; i < n; ++i) {
       tasks[i].filename = argv[1];
       tasks[i].seed = i + 1;
@@ -208,10 +246,13 @@ int main(int argc, char *argv[])
          exit(EXIT_FAILURE);
       }
    }
+
+   /* Wait for all threads to complete. */
    for (i = 0; i < n; ++i) {
       pthread_join(tasks[i].tid, NULL);
    }
 
+   /* Destroy the mutex. */
    pthread_mutex_destroy(&mutex);
 
    return EXIT_SUCCESS;
